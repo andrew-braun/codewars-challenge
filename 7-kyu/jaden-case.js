5f32839d9a9d739c5b51445b2987a84719956320
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-09-28 11:00:30 +0400

    jaden-case: Changed word[0] to charAt to handle empty strings

--- 7-kyu/jaden-case.js
+++ 7-kyu/jaden-case.js
@@ -10,7 +10,7 @@ Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 
 String.prototype.toJadenCase = function () {
 	return this.split(" ").map((word) => {
-        return word[0].toUpperCase() + word.slice(1);
+        return word.charAt(0).toUpperCase() + word.slice(1);
     })
     .join(" ")
 };
