173ebba5926da7240a38f383998466576e54986f
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-09-29 17:56:38 +0400

    new: Complete Square of Squares

---
+++ 7-kyu/square-of-squares.js
@@ -0,0 +1,16 @@
+/*
+Name: A Square of Squares
+Link: https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/javascript
+Description: Given an integral number, determine if it's a square number:
+In mathematics, a square number or perfect square is an integer that is the square of an integer; 
+in other words, it is the product of some integer with itself.
+The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+*/
+
+const isSquare = (num) => {
+    const sqrt = Math.sqrt(num);
+    return (sqrt=== Math.floor(sqrt))
+}
+
+console.log(isSquare(4));
+console.log(isSquare(3));
