f686c880fd43a8e898a8341dcae5d8b5f131e2c3
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-10-19 11:00:04 +0400

    new: Complete Build Tower challenge

---
+++ 6-kyu/build-tower.js
@@ -0,0 +1,60 @@
+/*
+Name: Build tower
+Link: https://www.codewars.com/kata/576757b1df89ecf5bd00073b/train/javascript
+Description: Build Tower
+
+Build Tower by the following given argument:
+number of floors (integer and always greater than 0).
+
+Tower block is represented as *
+
+    Python: return a list;
+    JavaScript: returns an Array;
+    C#: returns a string[];
+    PHP: returns an array;
+    C++: returns a vector<string>;
+    Haskell: returns a [String];
+    Ruby: returns an Array;
+    Lua: returns a Table;
+
+Have fun!
+
+for example, a tower of 3 floors looks like below
+
+[
+  '  *  ', 
+  ' *** ', 
+  '*****'
+]
+
+and a tower of 6 floors looks like below
+
+[
+  '     *     ', 
+  '    ***    ', 
+  '   *****   ', 
+  '  *******  ', 
+  ' ********* ', 
+  '***********'
+]
+
+*/
+
+function towerBuilder(nFloors) {
+	const tower = [];
+	let numSpaces = nFloors * 2 - 1;
+	let currentFloor = 1;
+
+	while (currentFloor < nFloors + 1) {
+		numBlocks = currentFloor * 2 - 1;
+		tower.push(
+			" ".repeat((numSpaces - numBlocks) / 2) +
+				"*".repeat(numBlocks) +
+				" ".repeat((numSpaces - numBlocks) / 2)
+		);
+		currentFloor++;
+	}
+	return tower;
+}
+
+console.log(towerBuilder(3));
